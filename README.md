ff7e45c7ed7c1522cf1d31d143a55d436c80b3eb
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-03-01 19:49:39 +0100

    Create setuptools boilerplate

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 litex_zephyr_export
 ===================
 
-Export [litex](https://github.com/enjoy-digital/litex) SoC definitions to [Zephyr](https://zephyrproject.org/) boards.
+Export [litex](https://github.com/enjoy-digital/litex) SoC definitions to [Zephyr](https://zephyrproject.org/) board files.
---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setup of Beckhoff BK5120 CANopen bus coupler test"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="litex_zephyr_export",
+    version="0.1",
+    author="Robert Wilbrandt",
+    author_email="robert@stamm-wilbrandt.de",
+    description="Export litex SoC definitions to Zephyr board files",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    scripts=["scripts/generate-zephyr-board"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
