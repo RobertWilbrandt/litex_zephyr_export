43cfa27d1f04a4247b4313c32b2afee5db139a7c
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-03-02 21:06:11 +0100

    Add initial draft for SoC configuration

---
+++ litex_zephyr_export/soc.py
@@ -0,0 +1,51 @@
+"""SoC configuration description that can be parsed from a litex export"""
+
+
+class SoC:
+    """A complete SoC configuration
+
+    :param name: Name of the SoC
+    :type name: str
+    :param vendor: Vendor name
+    :type vendor: str
+    :param description: SoC description or None
+    :type description: str
+    """
+
+    def __init__(self, name, vendor, description):
+        self.name = name
+        self.vendor = vendor
+        self.description = description
+
+        self.peripherals = []
+        self.memory_regions = []
+
+    def add_peripheral(self, peripheral):
+        """Add a peripheral to this SoC configuration
+
+        :param peripheral: Peripheral to be added
+        :type peripheral: Peripheral
+        """
+        self.peripherals += peripheral
+
+    def add_memory_region(self, memory_region):
+        """Add a memory region to this SoC configuration
+
+        :param memory_region: Memory region to be added
+        :type memory_region: MemoryRegion
+        """
+        self.memory_regions += memory_region
+
+
+class Peripheral:
+    """The configuration of a single peripheral"""
+
+    def __init__(self):
+        pass
+
+
+class MemoryRegion:
+    """One SoC memory region"""
+
+    def __init__(self):
+        pass
